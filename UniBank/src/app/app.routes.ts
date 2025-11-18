cb1140e90a098fb8dbb70994be9b07773692a993
Alxn2306 <223111621@upmh.edu.mx>
2025-11-18 08:59:51 -0600

    Actualizacion

--- UniBank/src/app/app.routes.ts
+++ UniBank/src/app/app.routes.ts
@@ -3,8 +3,6 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Register } from './pages/register/register';
 import { Logincliente } from './pages/logincliente/logincliente';
-import { Loginejecutivo } from './pages/loginejecutivo/loginejecutivo';
-import { Loginmanager } from './pages/loginmanager/loginmanager';
 import { ClienteLayout } from './pages/cliente/cliente-layout/cliente-layout';
 import { Micuenta } from './pages/cliente/micuenta/micuenta';
 import { Credito } from './pages/cliente/credito/credito';
@@ -27,8 +25,6 @@ export const routes: Routes = [
     {path: '', component: Home},
     {path: 'register', component: Register},
     {path: 'logincliente', component: Logincliente},
-    {path: 'loginejecutivo', component: Loginejecutivo},
-    {path: 'loginmanager', component: Loginmanager},
     {path: 'recuperar-password', component: RecuperarPassword},
 
     // === REDIRECCIONES PARA RUTAS PRINCIPALES ===
